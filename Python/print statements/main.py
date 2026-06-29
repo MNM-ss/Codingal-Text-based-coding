b46cf71d73cd91380142595940479120edce5fe0
MNM-ss <nusayba.manha4u@gmail.com>
2026-06-29 20:32:03 +0600

    python intro(staements)

---
+++ Python/print statements/main.py
@@ -0,0 +1,10 @@
+# use hashtag for a comment btw
+
+print("Hello, world!")
+
+print("My Name is manha Nusayba Mahmud.")
+
+print("I like painting, playing games and crafting")
+
+print("Did you know that the addition of 2026 and 365 is ", 2026 + 365)
+

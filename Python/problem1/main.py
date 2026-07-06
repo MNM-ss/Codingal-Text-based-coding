e98ea5e746dd7dd2023c17a5772e1c516d4218f1
MNM-ss <nusayba.manha4u@gmail.com>
2026-07-06 20:08:00 +0600

    data type

--- Python/problem1/main.py
+++ Python/problem1/main.py
@@ -5,5 +5,5 @@ date_word = "fifth"
 date_interger = 5
 year = 2012
 
-print("Hello, I'm Manha \nI was born in ", month_word," ", date_word, " ", year, ".")
+print("Hello, I'm Manha. \nI was born in", month_word, date_word, year, ".")
 print("\nOr you can say that I was born in ", date_interger, "/", month_integer, "/", year, ".")

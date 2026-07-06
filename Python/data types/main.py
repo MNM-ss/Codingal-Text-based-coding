c77666637b9bdd9c7b7922869726b3b2b4a2eb7c
MNM-ss <nusayba.manha4u@gmail.com>
2026-07-06 20:22:24 +0600

    type casting

--- Python/data types/main.py
+++ Python/data types/main.py
@@ -1,3 +1,5 @@
+#data type testing
+
 a = 5
 print("type of a: ", type(a))
 
@@ -9,3 +11,33 @@ print("type of c: ", type(c))
 
 d = True
 print("type of d: ", type(d))
+
+
+#type casing
+
+name = "Manha"
+age = 14
+weight = 48.5
+is_student = True
+
+#Printing diff variables and their data types
+
+print("Name: ", name)
+print("The type of data Name is: type(name)")
+
+print("Age: ", age)
+print("The type of data Age is: type(age)")
+
+print("Weight: ", weight)
+print("The type of data Weight is: type(weight)")
+
+# Type casting to convert the datatype of variables
+
+print("\n After type casting...")
+age = str(age)
+print(age)
+print("The data type of age is now : ", type(age))
+
+weight = int(weight)
+print(weight)
+print("the data type of weight is now ;", type(weight))

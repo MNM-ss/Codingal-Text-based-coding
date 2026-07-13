f7de6c32bddc299322e2e2d3b2f05f00c4a7028e
MNM-ss <nusayba.manha4u@gmail.com>
2026-07-13 20:09:17 +0600

    if else

---
+++ Python/if else/main.py
@@ -0,0 +1,11 @@
+num = 5
+if num > 0:
+    print(num, " is a positive integer")
+
+num = 0
+if num == 0:
+    print(num, "is equal to zero.")
+
+num = -6
+if num < 0:
+    print(num, "is a negative integer.")

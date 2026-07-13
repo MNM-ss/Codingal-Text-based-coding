1117ab2f90ef1e6119dd67290e85ce41f0e5783a
MNM-ss <nusayba.manha4u@gmail.com>
2026-07-13 20:21:48 +0600

    profit loss

--- Python/if else/main.py
+++ Python/if else/main.py
@@ -9,3 +9,17 @@ if num == 0:
 num = -6
 if num < 0:
     print(num, "is a negative integer.")
+
+
+
+
+
+cost_price = float(input("Please Enter the Actual Product price: "))
+sale_price = float(input("Please Enter the Product Sale price: "))
+
+if sale_price > cost_price:
+ profit = sale_price - cost_price
+ print("Total profit is  = {0}".format(profit))
+else:
+  loss = cost_price - sale_price
+  print("Total loss is  = {0}".format(loss))

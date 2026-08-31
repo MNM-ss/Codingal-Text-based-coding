42fd7b343901f54ca1a4b90fa2b8a685f507fea1
MNM-ss <nusayba.manha4u@gmail.com>
2026-08-31 20:31:38 +0600

    list2

--- Python/list/main.py
+++ Python/list/main.py
@@ -9,3 +9,17 @@ print(triple)
 alist = [100, 200, 300, 400, 500]
 alist = alist[::-1]
 print(alist, "\n")
+
+
+
+def match_words (words):
+    ctr = 0
+    lst = []
+    for word in words:
+     if len(word) > 1 and word [0] == word[-1]:
+        ctr += 1
+        lst.append(word)
+    print("List of words with first and last character same\n", lst)
+    return ctr
+count = match_words(['abc', 'cfc','xyz', 'aba', '1221'])
+print("Number of words having first and last character same:", count)
